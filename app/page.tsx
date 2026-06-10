a1bf8ecfb3569dd0adf07152f14efccac7da58fb
v0agent <it+v0agent@vercel.com>
2026-06-10 18:29:44 +0000

    feat: comment out Research component in page layout
    Commented out the Research component in the page layout for potential future use
    
    Co-authored-by: somang <2969379+somang@users.noreply.github.com>

--- app/page.tsx
+++ app/page.tsx
@@ -22,7 +22,7 @@ export default function Page() {
             </div>
             <News />
           </div>
-          <Research />
+          {/* <Research /> */}
           <Team />
           <Works preview />
           <Contact />
